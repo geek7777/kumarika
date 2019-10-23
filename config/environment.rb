133ab9e8fc3637f95c80b37fa4091f03f6a1e1a1
Shimol-Khan <se.shimolkhan@gmail.com>
2019-10-24 03:17:12 +0800

    Config Devise User

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'geek777',
+    :password => 'swseSWSE1',
+    :domain => 'kumarika.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
