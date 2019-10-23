01a74c5a329e08603d9a633accb75d9bb853cba2
Shimol-Khan <se.shimolkhan@gmail.com>
2019-10-24 03:29:57 +0800

    Contest CRUD

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -69,6 +69,6 @@ class ContestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contest_params
-      params.require(:contest).permit(:name, :description, :user_id)
+      params.require(:contest).permit(:name, :description, :user_id, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_191749) do
+ActiveRecord::Schema.define(version: 2019_10_23_192240) do
 
   create_table "contests", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2019_10_23_191749) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_contests_on_user_id"
   end
 
